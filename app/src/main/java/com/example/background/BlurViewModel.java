55478d67ca6ae6eb4035a7bbad224b58c6677c93
ArtemiyYarovenko <tekkenik@mail.ru>
2020-10-24 06:20:15 +0700

    Добавление запроса на работу при нажатии GO

--- app/src/main/java/com/example/background/BlurViewModel.java
+++ app/src/main/java/com/example/background/BlurViewModel.java
@@ -18,24 +18,33 @@ package com.example.background;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
 import android.app.Application;
 import android.net.Uri;
 import android.text.TextUtils;
+
+import com.example.background.workers.BlurWorker;
 
 public class BlurViewModel extends AndroidViewModel {
 
     private Uri mImageUri;
 
+
+
+    private WorkManager mWorkManager;
+    // BlurViewModel constructor
     public BlurViewModel(@NonNull Application application) {
         super(application);
+        mWorkManager = WorkManager.getInstance(application);
+        //...rest of the constructor
     }
 
-    /**
-     * Create the WorkRequest to apply the blur and save the resulting image
-     * @param blurLevel The amount to blur the image
-     */
-    void applyBlur(int blurLevel) {
 
+
+    void applyBlur(int blurLevel) {
+        mWorkManager.enqueue(OneTimeWorkRequest.from(BlurWorker.class));
     }
 
     private Uri uriOrNull(String uriString) {
